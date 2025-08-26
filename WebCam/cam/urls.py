098529e11210fbade3a61c8004c16d85de6dbbcd
IKirillka <77016004+IKirillka@users.noreply.github.com>
2025-08-26 23:47:57 +0500

    Delete WebCam directory

--- WebCam/cam/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_page, name='main'),
-    path('registration/', views.registration, name='registration'),
-    path('video_feed/', views.video_feed, name='video_feed'),
-]
